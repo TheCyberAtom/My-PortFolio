73b18dc6c36302959f03b9fd281cbbcea5441705
TheCyberAtom <rkm.rahulkmishra@gmail.com>
2023-09-18 20:11:07 +0530

    image optimisation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import HeroSection from '../components/HeroSection';
-import About from '../components/About';
-import Projects from '../components/Projects';
-import Footer from '../components/Footer';
-import { Element } from 'react-scroll';
+import React, { lazy, Suspense } from "react";
+import { Element } from "react-scroll";
+const Navbar = lazy(() => import("../components/Navbar"));
+const HeroSection = lazy(() => import("../components/HeroSection"));
+const About = lazy(() => import("../components/About"));
+const Projects = lazy(() => import("../components/Projects"));
+const Footer = lazy(() => import("../components/Footer"));
 
 const Home = () => {
   return (
     <>
-      <Navbar />
-      <Element name="home">
-        <HeroSection />
-      </Element>
-      <Element name="about">
-        <About />
-      </Element>
-      <Element name="projects">
-        <Projects />
-      </Element>
-      <Element name="contact">
-        <Footer />
-      </Element>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Navbar />
+        <Element name="home">
+          <HeroSection />
+        </Element>
+        <Element name="about">
+          <About />
+        </Element>
+        <Element name="projects">
+          <Projects />
+        </Element>
+        <Element name="contact">
+          <Footer />
+        </Element>
+      </Suspense>
     </>
   );
 };
